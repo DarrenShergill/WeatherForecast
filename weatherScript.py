c310e4a58311664b5bf3e444504c6201262af74e
Darren <darren@shergill.com>
2022-06-12 12:41:48 -0700

    cleaned up unused files

--- weatherScript.py
+++
@@ -1,8 +0,0 @@
-#location class containing input for zip/api gateway here?
-#set key value pair (LOCATION, 94432) in weatherScript edit config so we could delete .env file (this is what prints the zipcode)
-#having another python or shell script used to learn how to make calls to the weather api. I need to hard code my address
-    #focus on getting results (whatever information i want. humidity, weather, whatever)
-
-
-
-
